d5882e1194a1d3432e27d4cd7c286b5780b12599
Udbhaav Mudgil <91746508+umudgil@users.noreply.github.com>
2026-01-05 11:03:05 -0500

    Update Queries_Quality_and_Percentage.sql

--- easy/Queries_Quality_and_Percentage.sql
+++ easy/Queries_Quality_and_Percentage.sql
@@ -1,1 +1,3 @@
-
+SELECT query_name, ROUND(AVG(rating/position),2) AS quality, ROUND(100*sum(case when rating < 3 then 1 else 0 end)/count(*),2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
