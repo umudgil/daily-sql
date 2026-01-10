c235b165e4696f65838501d13113cf3d62747397
Udbhaav Mudgil <91746508+umudgil@users.noreply.github.com>
2026-01-10 16:24:59 -0500

    Update List_The_Products_Ordered_In_a_Period.sql

--- easy/List_The_Products_Ordered_In_a_Period.sql
+++ easy/List_The_Products_Ordered_In_a_Period.sql
@@ -1,1 +1,7 @@
-
+SELECT p.product_name, SUM(o.unit) as unit
+FROM ORDERS o
+LEFT JOIN Products p
+    ON o.product_id = p.product_id
+WHERE order_date LIKE '2020-02-%'
+GROUP BY o.product_id
+HAVING SUM(unit) >= 100;
